5eea21910748fe35668a43fd10380e7d45f4e015
sabenih726 <sabenih726@gmail.com>
2025-09-11 15:21:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -333,18 +333,8 @@ with active_tab[3]:
                 st.success(f"Barang '{barang_pilihan}' berhasil dihapus dari stok!")
 
 st.markdown("""
-<style>
-.footer {
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    text-align: center;
-    color: grey;
-    font-size: 13px;
-    padding: 5px;
-}
-</style>
-<div class="footer">
+---
+<div style='text-align:center; color:grey; font-size:13px;'>
     Created by <b>Facility Maintenance</b>
 </div>
 """, unsafe_allow_html=True)
